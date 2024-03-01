58d0ae8f8fd9c93d1862d6ccb9b36e9a6d663239
syedubaidshah <obaidshah0099@gmail.com>
2024-03-01 23:24:35 +0500

    operators

---
+++ operators&conditionalstatement/app.js
@@ -0,0 +1,42 @@
+//Arithmetic operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+//Unary Operator
+// let a = 5;
+// let b = 2;
+// console.log("a = ", a, " & b = ", b);
+// console.log("--a = ", a--);
+// console.log("a = ", a);
+
+// Assigment Operators
+// let a = 5;
+// let b = 2;
+// a += 4;
+// console.log("a = ", a);
+// a -= 4;
+// console.log("a = ", a);
+
+// comparoson Operators
+// let a = 5;
+// let b = 2;
+// console.log("a == b" , a == b)
+// console.log("a != b" , a != b)
+
+// let a = 5;
+// let b = "5";
+// console.log("a === b" , a === b)
+// console.log("a !== b" , a !== b)
+
+//Logical Operators
+let a = 5;
+let b = 2;
+console.log("cond1 && cond2 = ", a < b && a === 6);
