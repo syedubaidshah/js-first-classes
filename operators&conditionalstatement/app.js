1d008420e297ba8b863c8cfde3c2d9ad5e5b75f7
syedubaidshah <obaidshah0099@gmail.com>
2024-03-03 20:57:50 +0500

    Update app.js

--- operators&conditionalstatement/app.js
+++ operators&conditionalstatement/app.js
@@ -10,12 +10,21 @@
 // console.log("a % b = ", a % b);
 // console.log("a ** b = ", a ** b);
 
+
+
+
+
+
 //Unary Operator
 // let a = 5;
 // let b = 2;
 // console.log("a = ", a, " & b = ", b);
 // console.log("--a = ", a--);
 // console.log("a = ", a);
+
+
+
+
 
 // Assigment Operators
 // let a = 5;
@@ -24,6 +33,9 @@
 // console.log("a = ", a);
 // a -= 4;
 // console.log("a = ", a);
+
+
+
 
 // comparoson Operators
 // let a = 5;
@@ -36,20 +48,40 @@
 // console.log("a === b" , a === b)
 // console.log("a !== b" , a !== b)
 
+
+
+
+
 //Logical Operators
 // let a = 5;
 // let b = 2;
 // console.log("cond1 && cond2 = ", a < b && a === 6);
 
+
+
+
+
 /* Conditional Statement
 to implement some condition in the code */
 
-let age = 22;
+// let age = 18;
 
-if (age >= 18) {
-    console.log("you can vote");
-}
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+// else {
+//     console.log("you CANNOT vote");
+// }
 
-if (age < 18) {
-    console.log("you CANNOT vote");
-}
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color ="black";
+// }
+// if(mode ==="light"){
+//     color = "white";
+// }
+// console.log(color);
+
