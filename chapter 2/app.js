aeee9a9a84472d31bb246e640352c39ea1d6d4a7
syedubaidshah <obaidshah0099@gmail.com>
2024-03-16 01:06:32 +0500

    chapter2

---
+++ chapter 2/app.js
@@ -0,0 +1,62 @@
+// 01: Declare a variable Called Username.
+let username = "Syed Ubaid Shah";
+
+
+
+
+// 02: Declare a variable called myName & assign to it a string that represent your fullname.
+let myName = "Syed Ubaid Shah";
+console.log(myName);
+
+
+
+
+
+// 03: Write Script to
+// Decelare a js variable, tittle message
+let message;
+// Assign "Hello World" to variable message
+message = "Hello World";
+// Display the message in Alert box
+alert(message);
+
+
+
+
+
+// 04: write a script to save student's bio data in js variable and show the data in alert boxes
+let yourName = prompt("Enter your name");
+let yourAge = prompt("Enter your Age");
+let qualification = prompt("Enter your Qualification");
+alert(yourName);
+alert(yourAge);
+alert(qualification);
+
+
+
+
+// 05: write a script to display the following alert using one JS variable.
+let alertMessage = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(alertMessage);
+
+
+
+
+// 06:
+let email = "obaidshah@gmail.com";
+let message = "Your email adress is: " + email;
+alert(message);
+
+
+
+
+// 07:
+let book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: " + book);
+
+
+
+
+// 08:
+let name = "Yah! I can write HTML content through Javascript";
+document.write(name);
