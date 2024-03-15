a85c61dfb1c5cb35079d92fd04415bd6369f2972
syedubaidshah <obaidshah0099@gmail.com>
2024-03-16 01:29:45 +0500

    chapter 4

---
+++ chapter 4/app.js
@@ -0,0 +1,20 @@
+// 01:
+let a = "abc"
+let b = "abc"
+let c = "abc"
+console.log([a, b, c])
+
+
+// 02:
+// Legal variable names:
+let myVariable;
+let _underscoreVariable;
+let $dollarSignVariable;
+let anotherVariable123;
+let camelCaseVariable;
+// ILLegal variable names:
+let 123variable;
+let my-variable ;
+let my variable;
+let my.variable;
+let var;
